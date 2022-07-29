e518cda7408e072302c294202e33535db49d150d
hrtd <hrtd@DESKTOP-196G722>
2022-07-29 23:50:06 +0100

    Add project files.

---
+++ Models/WorkItem.cs
@@ -0,0 +1,9 @@
+﻿namespace MyApplication.Models
+{
+    public class WorkItem
+    { 
+         public string Name { get; set; }
+         public string Selector { get; set; }
+         public string Description { get; set; }
+    }
+}
